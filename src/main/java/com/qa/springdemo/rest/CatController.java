7fb581b5f398463bb2842521e1403b1535edc1a8
AelinaXY <Lauren@2infinity.net>
2023-04-19 11:48:57 +0100

    Add full Cat Crud

--- src/main/java/com/qa/springdemo/rest/CatController.java
+++ src/main/java/com/qa/springdemo/rest/CatController.java
@@ -1,16 +1,20 @@
 package com.qa.springdemo.rest;
 
+import com.qa.springdemo.rest.domain.Cat;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.qa.springdemo.rest.domain.Cat;
 @RestController
 public class CatController {
 
@@ -22,26 +26,40 @@ public class CatController {
     }
 
     @PostMapping("/create")
-    public Cat createCat(@RequestBody Cat e) {
-        this.catList.add(e);
-        return this.catList.get(catList.size() - 1);
+    public ResponseEntity<Cat> createCat(@RequestBody Cat e) {
+        catList.add(e);
+        Cat catCreated = catList.get(catList.size() - 1);
+        return new ResponseEntity<>(catCreated, HttpStatus.CREATED);
+        // return this.catList.get(catList.size() - 1);
     }
 
     @GetMapping("/getAll")
-    public List<Cat> getAll()
-    {
+    public List<Cat> getAll() {
         return catList;
     }
 
     @GetMapping("/get/{id}")
-    public Cat get(@PathVariable int id)
-    {
+    public Cat get(@PathVariable int id) {
         return catList.get(id);
     }
 
     @DeleteMapping("/remove/{id}")
-    public Cat remove(@PathVariable int id)
-    {
+    public Cat remove(@PathVariable int id) {
         return catList.remove(id);
+    }
+
+    @PatchMapping("/update/{id}")
+    public Cat updateCat(
+            @PathVariable int id,
+            @RequestParam(name = "hasWhiskers", required = false) boolean hasWhiskers,
+            @RequestParam(name = "evil", required = false) boolean evil,
+            @RequestParam(name = "name", required = false) String name,
+            @RequestParam(name = "length", required = false) int length) {
+        Cat updateCat = catList.get(id);
+        updateCat.setHasWhiskers(hasWhiskers);
+        updateCat.setEvil(evil);
+        updateCat.setName(name);
+        updateCat.setLength(length);
+        return updateCat;
     }
 }
