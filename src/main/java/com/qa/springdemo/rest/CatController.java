17f9434abbff02e06c426f27b24ca36951f2627d
AelinaXY <Lauren@2infinity.net>
2023-04-19 10:29:49 +0100

    Add Delete and Get(ID)

--- src/main/java/com/qa/springdemo/rest/CatController.java
+++ src/main/java/com/qa/springdemo/rest/CatController.java
@@ -3,7 +3,9 @@ package com.qa.springdemo.rest;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -29,5 +31,17 @@ public class CatController {
     public List<Cat> getAll()
     {
         return catList;
+    }
+
+    @GetMapping("/get/{id}")
+    public Cat get(@PathVariable int id)
+    {
+        return catList.get(id);
+    }
+
+    @DeleteMapping("/remove/{id}")
+    public Cat remove(@PathVariable int id)
+    {
+        return catList.remove(id);
     }
 }
